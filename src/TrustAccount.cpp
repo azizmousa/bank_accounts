1157004b3dbb50883e6ab1a02653d4803ac634a8
azizmousa <azizmousa1010@gmail.com>
2020-12-13 00:44:51 +0200

    add TrustAccount implementation

---
+++ src/TrustAccount.cpp
@@ -0,0 +1,37 @@
+
+#include "bank_accounts/TrustAccount.h"
+
+/*
+ * TrustAccount(std::string name, double balance, double bonus, int withdraw_rate)
+ * class initialilzer
+*/
+TrustAccount::TrustAccount(std::string name, double balance, double bonus,
+double bonus_check, double withdraw_amount_limit,
+ int withdraw_limit)
+    :Account(name, balance), bonus{bonus}, 
+    bonus_check{bonus_check},
+    withdraw_amount_limit{withdraw_amount_limit},
+    withdraw_limit{withdraw_limit}{
+}
+
+/*
+ * bool deposit(double amount)
+ * method to deposit money to the account with a bonus
+*/
+bool TrustAccount::deposit(double amount){
+    if(amount > this->bonus_check)
+        amount += this->bonus;
+    return Account::deposit(amount);
+}
+
+/*
+ * bool withdraw(double amount)
+ * method to withdraw money with checking limits
+*/
+bool TrustAccount::withdraw(double amount){
+    double amount_limit = (withdraw_amount_limit/100) * this->balance;
+    if(amount > amount_limit || this->withdraw_times_limit == 0)
+        return false;
+    this->withdraw_times_limit--;
+    return Account::withdraw(amount);
+}
