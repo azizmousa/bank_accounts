a8270ca3f7705ca5515cb7e24c0ce558f6e7f7a3
azizmousa <azizmousa1010@gmail.com>
2020-12-12 23:22:12 +0200

    optimiz CheckingAccount header

--- include/bank_accounts/CheckingAccount.h
+++ include/bank_accounts/CheckingAccount.h
@@ -10,15 +10,12 @@ class CheckingAccount: public Account{
 private:
     static constexpr char *DEF_NAME = const_cast<char *>("UNNAMED CHECKING ACCOUNT");
     static constexpr double DEF_BALANCE = 0.0;
-    static constexpr double DEF_FEE = 0.0;
+    static constexpr double DEF_FEE = 1.50;
 protected:
     double fee;
 public:
     CheckingAccount(std::string name=DEF_NAME, double balance=DEF_BALANCE, double fee=DEF_FEE);
-    bool deposit(double amount);
     bool withdraw(double amount);
-    double get_blanace()const;
-    std::string get_account_name()const;
 };
 
 #endif // _CHECKING_ACCOUNT_H
