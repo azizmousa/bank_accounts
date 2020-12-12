f3b7b5add5638d58122fe5c56968c2e8eeaa010f
azizmousa <azizmousa1010@gmail.com>
2020-12-12 22:11:46 +0200

    Add saving accounts helper functions implementatio

--- src/Account_util.cpp
+++ src/Account_util.cpp
@@ -39,3 +39,43 @@ void withdraw(std::vector<Account> &accounts, double amount){
             std::cout << acc.get_account_name() << " withdraw operation faild!" << std::endl;
     }
 }
+
+
+/*
+ * void display(const std::vector<SavingAccount> &saving_accounts)
+ * helper function to display list of SavingAccounts
+*/
+void display(const std::vector<SavingAccount> &saving_accounts){
+    std::cout << "===========SavingAccount==============" << std::endl;
+    for(SavingAccount acc : saving_accounts){
+        std::cout << acc << std::endl;
+    }
+    std::cout << "================================" << std::endl;
+}
+
+
+/*
+ * void deposit(std::vector<SavingAccount> &saving_accounts, double amount)
+ * helper function to deposit an amount to list of SavingAccounts
+*/
+void deposit(std::vector<SavingAccount> &saving_accounts, double amount){
+    for(SavingAccount &acc : saving_accounts){
+        if(acc.deposit(amount))
+            std::cout << acc.get_account_name() << " deposit: " << amount <<std::endl;
+        else 
+            std::cout << acc.get_account_name() << " deposit operation faild!" << std::endl;
+    }
+}
+
+/*
+ * void withdraw(std::vector<SavingAccount> &saving_accounts, double amount)
+ * helper function to withdraw an amount of money from list of Accounts
+*/
+void withdraw(std::vector<SavingAccount> &saving_accounts, double amount){
+    for(SavingAccount &acc:saving_accounts){
+        if(acc.withdraw(amount))
+            std::cout << acc.get_account_name() << " withdraw: " << amount << std::endl;
+        else
+            std::cout << acc.get_account_name() << " withdraw operation faild!" << std::endl;
+    }
+}
