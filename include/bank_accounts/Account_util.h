8c3eef7499cd55c94b186526b86c11a5926bdd90
azizmousa <azizmousa1010@gmail.com>
2020-12-13 01:04:51 +0200

    add trust, checking account helper functions

--- include/bank_accounts/Account_util.h
+++ include/bank_accounts/Account_util.h
@@ -4,6 +4,8 @@
 
 #include "bank_accounts/Account.h"
 #include "bank_accounts/SavingAccount.h"
+#include "bank_accounts/CheckingAccount.h"
+#include "bank_accounts/TrustAccount.h"
 
 // utility helper functions for Account class
 void display(const std::vector<Account> &accounts);
@@ -15,4 +17,13 @@ void display(const std::vector<SavingAccount> &saving_accounts);
 void deposit(std::vector<SavingAccount> &saving_accounts, double amount);
 void withdraw(std::vector<SavingAccount> &saving_accounts, double amount);
 
+// utility helper functions for CheckingAccount class
+void display(const std::vector<CheckingAccount> &checking_accounts);
+void deposit(std::vector<CheckingAccount> &checking_accounts, double amount);
+void withdraw(std::vector<CheckingAccount> &checking_accounts, double amount);
+
+// utility helper functions for TrustAccount class
+void display(const std::vector<TrustAccount> &trust_accounts);
+void deposit(std::vector<TrustAccount> &trust_accounts, double amount);
+void withdraw(std::vector<TrustAccount> &trust_accounts, double amount);
 #endif //_ACCOUNT_UTIL_H
