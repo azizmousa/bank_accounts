2d08f0b4439d1ae0d3db8b8e8eda3d7f39d2d366
azizmousa <azizmousa1010@gmail.com>
2020-12-13 17:45:48 +0200

    change the vector type

--- include/bank_accounts/Account_util.h
+++ include/bank_accounts/Account_util.h
@@ -8,22 +8,8 @@
 #include "bank_accounts/TrustAccount.h"
 
 // utility helper functions for Account class
-void display(const std::vector<Account> &accounts);
-void deposit(std::vector<Account> &accounts, double amount);
-void withdraw(std::vector<Account> &accounts, double amount);
+void display(const std::vector<Account*> &accounts);
+void deposit(std::vector<Account*> &accounts, double amount);
+void withdraw(std::vector<Account*> &accounts, double amount);
 
-// utility helper functions for SavingAccount class
-void display(const std::vector<SavingAccount> &saving_accounts);
-void deposit(std::vector<SavingAccount> &saving_accounts, double amount);
-void withdraw(std::vector<SavingAccount> &saving_accounts, double amount);
-
-// utility helper functions for CheckingAccount class
-void display(const std::vector<CheckingAccount> &checking_accounts);
-void deposit(std::vector<CheckingAccount> &checking_accounts, double amount);
-void withdraw(std::vector<CheckingAccount> &checking_accounts, double amount);
-
-// utility helper functions for TrustAccount class
-void display(const std::vector<TrustAccount> &trust_accounts);
-void deposit(std::vector<TrustAccount> &trust_accounts, double amount);
-void withdraw(std::vector<TrustAccount> &trust_accounts, double amount);
 #endif //_ACCOUNT_UTIL_H
