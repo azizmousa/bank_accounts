98b01d758dd22e9c6623f54131044e934a9c9d75
azizmousa <azizmousa1010@gmail.com>
2020-12-13 15:55:47 +0200

    refactor to be abstract class

--- src/Account.cpp
+++ src/Account.cpp
@@ -9,27 +9,27 @@ Account::Account(std::string name, double balance): name{name}, balance{balance}
 
 }
 
-/*
- * deopsit(double amount)
- * method to add amount of money to the base balance
-*/
-bool Account::deposit(double amount){
-    if(amount < 0)
-        return false;
-    this->balance += amount;
-    return true;
-}
+// /*
+//  * deopsit(double amount)
+//  * method to add amount of money to the base balance
+// */
+// bool Account::deposit(double amount){
+//     if(amount < 0)
+//         return false;
+//     this->balance += amount;
+//     return true;
+// }
 
-/*
- * withdraw(double amount)
- * method to decrease amount of money from the base balance
-*/
-bool Account::withdraw(double amount){
-    if(amount > balance || amount < 0)
-       return false;
-    this->balance -= amount; 
-    return true;
-}
+// /*
+//  * withdraw(double amount)
+//  * method to decrease amount of money from the base balance
+// */
+// bool Account::withdraw(double amount){
+//     if(amount > balance || amount < 0)
+//        return false;
+//     this->balance -= amount; 
+//     return true;
+// }
 
 /*
  * get_balance()const
@@ -48,10 +48,29 @@ std::string Account::get_account_name()const{
 }
 
 /*
- * std::ostream &operator<<(std::ostream &out, const Account &account)
- * insertion operator overload to write the account data
+ * bool withdtaw_assurance(double amount) const
+ * method to check if the amount is valid value
 */
-std::ostream &operator<<(std::ostream &out, const Account &account){
-    out << "[Name: " << account.name << "\tBalance: " << account.balance << "]";
-    return out;
+bool Account::withdraw_assurance(double amount)const{
+    if(amount > this->balance || amount < 0)
+        return false;
+    return true;
 }
+
+/*
+ * bool deposit_assurance(double amount) const
+ * method to check if the amount is valid value
+*/
+bool Account::deposit_assurance(double amount)const{
+    if(amount < 0)
+        return false;
+    return true;
+}
+// /*
+//  * std::ostream &operator<<(std::ostream &out, const Account &account)
+//  * insertion operator overload to write the account data
+// */
+// std::ostream &operator<<(std::ostream &out, const Account &account){
+//     out << "[Name: " << account.name << "\tBalance: " << account.balance << "]";
+//     return out;
+// }
