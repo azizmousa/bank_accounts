48c9606c2d11f4a8deecabb1fea750dd3aa3a4eb
azizmousa <azizmousa1010@gmail.com>
2020-12-13 16:00:11 +0200

    stop the Account base helper functions

--- src/Account_util.cpp
+++ src/Account_util.cpp
@@ -1,44 +1,44 @@
 #include <iostream>
 #include "bank_accounts/Account_util.h"
 
-/*
- * void display(const std::vector<Account> &accounts)
- * helper function to display list of Account
-*/
-void display(const std::vector<Account> &accounts){
-    std::cout << "===========Account==============" << std::endl;
-    for(Account acc : accounts){
-        std::cout << acc << std::endl;
-    }
-    std::cout << "================================" << std::endl;
-}
+// /*
+//  * void display(const std::vector<Account> &accounts)
+//  * helper function to display list of Account
+// */
+// void display(const std::vector<Account> &accounts){
+//     std::cout << "===========Account==============" << std::endl;
+//     for(Account acc : accounts){
+//         std::cout << acc << std::endl;
+//     }
+//     std::cout << "================================" << std::endl;
+// }
 
 
-/*
- * void deposit(std::vector<Account> &accounts, double amount)
- * helper function to deposit an amount to list of Account
-*/
-void deposit(std::vector<Account> &accounts, double amount){
-    for(Account &acc : accounts){
-        if(acc.deposit(amount))
-            std::cout << acc.get_account_name() << " deposit: " << amount <<std::endl;
-        else 
-            std::cout << acc.get_account_name() << " deposit operation faild!" << std::endl;
-    }
-}
+// /*
+//  * void deposit(std::vector<Account> &accounts, double amount)
+//  * helper function to deposit an amount to list of Account
+// */
+// void deposit(std::vector<Account> &accounts, double amount){
+//     for(Account &acc : accounts){
+//         if(acc.deposit(amount))
+//             std::cout << acc.get_account_name() << " deposit: " << amount <<std::endl;
+//         else 
+//             std::cout << acc.get_account_name() << " deposit operation faild!" << std::endl;
+//     }
+// }
 
-/*
- * void withdraw(std::vector<Account> &accounts, double amount)
- * helper function to withdraw an amount of money from list of Account
-*/
-void withdraw(std::vector<Account> &accounts, double amount){
-    for(Account &acc:accounts){
-        if(acc.withdraw(amount))
-            std::cout << acc.get_account_name() << " withdraw: " << amount << std::endl;
-        else
-            std::cout << acc.get_account_name() << " withdraw operation faild!" << std::endl;
-    }
-}
+// /*
+//  * void withdraw(std::vector<Account> &accounts, double amount)
+//  * helper function to withdraw an amount of money from list of Account
+// */
+// void withdraw(std::vector<Account> &accounts, double amount){
+//     for(Account &acc:accounts){
+//         if(acc.withdraw(amount))
+//             std::cout << acc.get_account_name() << " withdraw: " << amount << std::endl;
+//         else
+//             std::cout << acc.get_account_name() << " withdraw operation faild!" << std::endl;
+//     }
+// }
 
 
 /*
@@ -108,7 +108,7 @@ void deposit(std::vector<CheckingAccount> &checking_accounts, double amount){
 }
 
 /*
- * void withdraw(std::vector<Account> &accounts, double amount)
+ * void withdraw(std::vector<CheckingAccount> &checking_accounts, double amount)
  * helper function to withdraw an amount of money from list of Account
 */
 void withdraw(std::vector<CheckingAccount> &checking_accounts, double amount){
