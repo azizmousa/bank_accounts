8efde76562e43e7dbfd9415a1edee903d5980895
azizmousa <azizmousa1010@gmail.com>
2020-12-13 16:17:08 +0200

    add print interface

---
+++ include/bank_accounts/I_Print.h
@@ -0,0 +1,12 @@
+#ifndef _I_PRINT_H
+#define _I_PRINT_H
+
+#include<iostream>
+
+class I_Print{
+    friend std::ostream &operator<<(std::ostream &out, I_Print &i_print);
+public:
+    virtual ~I_Print() = default;
+    virtual void print(std::ostream &out);
+};
+#endif//_I_PRINT_H
